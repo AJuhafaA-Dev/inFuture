42de3a0e3b153211e120afa62aef0d54eab528bd
AJuhafaA-Dev <jabinalam19@gmail.com>
2026-05-08 23:23:58 +0600

    0.1.0.0-added plastic bottle timelines and all, updated colors and visuals, added slight animations tho more is needed

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,6 +1,4 @@
 function cardclicked(years, itemName) {
   document.getElementById("selection-screen").style.display = "none";
-  document.getElementById("plasticBottle-screen").style.display = "none";
-  let futureYear = 2026 + years;
-  document.getElementById("main-text").innerText = `CURRENT YEAR ${futureYear}`;
+  document.getElementById("plasticBottle-screen").style.display = "block";
 }
