6c906f8d5c30a67b5bc635174de03b0a98313785
AJuhafaA-Dev <jabinalam19@gmail.com>
2026-05-12 21:40:10 +0600

    0.2.0.0-added home button, 2 new items and their cards, added basic responsiveness to some views

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,4 +1,25 @@
-function cardclicked(years, itemName) {
+function cardclicked() {
   document.getElementById("selection-screen").style.display = "none";
   document.getElementById("plasticBottle-screen").style.display = "block";
+  window.scrollTo(0, 0);
+}
+function cardclicked2() {
+  document.getElementById("selection-screen").style.display = "none";
+  document.getElementById("plasticBag-screen").style.display = "block";
+  window.scrollTo(0, 0);
+}
+function cardclicked3() {
+  document.getElementById("selection-screen").style.display = "none";
+  document.getElementById("glass-bottle-screen").style.display = "block";
+  window.scrollTo(0, 0);
+}
+function goHome() {
+  const screens = document.querySelectorAll(".item-screen");
+  screens.forEach((screen) => {
+    screen.style.display = "none";
+  });
+
+  document.getElementById("selection-screen").style.display = "block";
+
+  window.scrollTo(0, 0);
 }
